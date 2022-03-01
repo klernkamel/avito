47ca9a0f78bf87aa1092b9eadb4c899d6c4e66ed
klernkamel <klernkamel@mail.ru>
2022-03-01 17:39:10 +0000

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { Layout, Menu, Breadcrumb, Descriptions, Row, Col, Card, Divider, Calendar, List, Avatar, Statistic, Steps } from 'antd';
+import { Layout, Menu, Breadcrumb, Descriptions, Row, Col, Card, Divider, Calendar, List, Avatar, Statistic, Steps, Button, Input, Space, Select } from 'antd';
 import {
   DesktopOutlined,
   PieChartOutlined,
@@ -9,12 +9,32 @@ import {
   TeamOutlined,
   UserOutlined,
   StepForwardOutlined,
-  ArrowUpOutlined
+  ArrowUpOutlined,
+  HeartFilled,
+  AudioOutlined
 } from '@ant-design/icons';
 
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
+const { Search } = Input;
+const { Option } = Select;
+const { Meta } = Card;
+
+function handleChange(value: string) {
+  console.log(`selected ${value}`);
+}
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+    }}
+  />
+);
+
+const onSearch = (value: string) => console.log(value);
 
 const data = [
   {
@@ -37,115 +57,219 @@ function App() {
   const [collapsed, setCollapsed] = useState(false)
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={(collapse) => { setCollapsed(collapse) }}>
-        <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            Option 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Option 2
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9" icon={<FileOutlined />}>
-            Files
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className='site-layout'>
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            <>
-              <Row>
-                <Col span={24}>
-                  <Card title="Card title" bordered={false}>
-                    <Steps current={1}>
-                      <Step title="Finished" description="This is a description." />
-                      <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
-                      <Step title="Waiting" description="This is a description." />
-                    </Steps>
-                  </Card>
-                </Col>
-              </Row>
-              <Divider />
-              <Row gutter={16}>
-                <Col span={12}><Card title="Calendar" bordered={false}>
-                  <Calendar fullscreen={false} />
-                </Card></Col>
-                <Col span={12}><Card title="List" bordered={false}>
-                  <List
-                    itemLayout="horizontal"
-                    dataSource={data}
-                    renderItem={item => (
-                      <List.Item>
-                        <List.Item.Meta
-                          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                          title={<a href="https://ant.design">{item.title}</a>}
-                          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                        />
-                      </List.Item>
-                    )}
-                  />
-                </Card></Col>
-              </Row>
-              <Divider />
-              <Row gutter={16}>
-                <Col span={8}><Card title="Card title" bordered={false}>
-                  <Statistic
-                    title="Active"
-                    value={11.28}
-                    precision={2}
-                    valueStyle={{ color: '#3f8600' }}
-                    prefix={<ArrowUpOutlined />}
-                    suffix="%"
-                  />
-                </Card></Col>
-                <Col span={8}><Card title="Card title" bordered={false}>
-                  <Statistic
-                    title="Active"
-                    value={11.28}
-                    precision={2}
-                    valueStyle={{ color: '#3f8600' }}
-                    prefix={<ArrowUpOutlined />}
-                    suffix="%"
-                  />
-                </Card></Col>
-                <Col span={8}><Card title="Card title" bordered={false}>
-                  <Statistic
-                    title="Active"
-                    value={11.28}
-                    precision={2}
-                    valueStyle={{ color: '#3f8600' }}
-                    prefix={<ArrowUpOutlined />}
-                    suffix="%"
-                  />
-                </Card></Col>
-              </Row>
-              <Row>
-                <Col span={6}>col-6</Col>
-                <Col span={6}>col-6</Col>
-                <Col span={6}>col-6</Col>
-                <Col span={6}>col-6</Col>
-              </Row>
-            </>,
+      <Header>
+        <Row>
+          <Col span={6}>
+            <Menu theme="dark" mode="horizontal">
+              <Menu.Item>For bussines</Menu.Item>
+              <Menu.Item>Shops</Menu.Item>
+              <Menu.Item>Support</Menu.Item>
+            </Menu>
+          </Col>
+          <Col span={18}>
+            <Menu theme="dark" mode="horizontal" style={{ float: 'right' }}>
+              <Menu.Item><HeartFilled /></Menu.Item>
+              <Menu.Item>Sign In Sign Up</Menu.Item>
+              <Menu.Item><Button type="primary">Place an ad</Button></Menu.Item>
+            </Menu>
+          </Col>
+        </Row>
+        <Row gutter={45} align="bottom">
+          <Col>
+            <img width={120} src={'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Synergy-Logo-Large.png/640px-Synergy-Logo-Large.png'} />
+          </Col>
+          Cars
+          <Col>
+            Real Estate
+          </Col>
+          Work
+          <Col>
+            Services
+          </Col>
+        </Row>
+        <Input.Group>
+          <Row gutter={4}>
+            <Col span={6}>
+              <Select
+                mode="multiple"
+                style={{ width: '100%' }}
+                placeholder="select one country"
+                defaultValue={'china'}
+                onChange={handleChange}
+                optionLabelProp="label"
+              >
+                <Option value="china" label="China">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="China">
+                      🇨🇳
+                    </span>
+                    China (中国)
+                  </div>
+                </Option>
+                <Option value="usa" label="USA">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="USA">
+                      🇺🇸
+                    </span>
+                    USA (美国)
+                  </div>
+                </Option>
+                <Option value="japan" label="Japan">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="Japan">
+                      🇯🇵
+                    </span>
+                    Japan (日本)
+                  </div>
+                </Option>
+                <Option value="korea" label="Korea">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="Korea">
+                      🇰🇷
+                    </span>
+                    Korea (韩国)
+                  </div>
+                </Option>
+              </Select>
+            </Col>
+            <Col>
+              <Search
+                placeholder="input search text"
+                allowClear
+                enterButton="Search"
+                onSearch={onSearch}
+              />
+            </Col>
+            <Col span={6}>
+              <Select
+                mode="multiple"
+                style={{ width: '100%' }}
+                placeholder="select one country"
+                defaultValue={'china'}
+                onChange={handleChange}
+                optionLabelProp="label"
+              >
+                <Option value="china" label="China">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="China">
+                      🇨🇳
+                    </span>
+                    China (中国)
+                  </div>
+                </Option>
+                <Option value="usa" label="USA">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="USA">
+                      🇺🇸
+                    </span>
+                    USA (美国)
+                  </div>
+                </Option>
+                <Option value="japan" label="Japan">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="Japan">
+                      🇯🇵
+                    </span>
+                    Japan (日本)
+                  </div>
+                </Option>
+                <Option value="korea" label="Korea">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="Korea">
+                      🇰🇷
+                    </span>
+                    Korea (韩国)
+                  </div>
+                </Option>
+              </Select>
+            </Col>
+            <Col span={6}>
+              <Select
+                mode="multiple"
+                style={{ width: '100%' }}
+                placeholder="select one country"
+                defaultValue={'china'}
+                onChange={handleChange}
+                optionLabelProp="label"
+              >
+                <Option value="china" label="China">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="China">
+                      🇨🇳
+                    </span>
+                    China (中国)
+                  </div>
+                </Option>
+                <Option value="usa" label="USA">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="USA">
+                      🇺🇸
+                    </span>
+                    USA (美国)
+                  </div>
+                </Option>
+                <Option value="japan" label="Japan">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="Japan">
+                      🇯🇵
+                    </span>
+                    Japan (日本)
+                  </div>
+                </Option>
+                <Option value="korea" label="Korea">
+                  <div className="demo-option-label-item">
+                    <span role="img" aria-label="Korea">
+                      🇰🇷
+                    </span>
+                    Korea (韩国)
+                  </div>
+                </Option>
+              </Select>
+            </Col>
+          </Row>
+          <div style={{ margin: '55px 0px 0px 0px' }}>
+            <Row gutter={16}>
+              <Col span={3}>
+                <Card
+                  hoverable
+                  style={{ width: '100%' }}
+                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  <Meta title="Europe Street beat" description="www.instagram.com" />
+                </Card>
+              </Col>
+              <Col span={3}>
+                <Card
+                  hoverable
+                  style={{ width: '100%' }}
+                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  <Meta title="Europe Street beat" description="www.instagram.com" />
+                </Card>
+              </Col>
+              <Col span={3}>
+                <Card
+                  hoverable
+                  style={{ width: '100%' }}
+                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  <Meta title="Europe Street beat" description="www.instagram.com" />
+                </Card>
+              </Col>
+              <Col span={3}>
+                <Card
+                  hoverable
+                  style={{ width: '100%' }}
+                  cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  <Meta title="Europe Street beat" description="www.instagram.com" />
+                </Card>
+              </Col>
+            </Row>
           </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-      </Layout>
-    </Layout>
+        </Input.Group>
+      </Header>
+    </Layout >
   );
 }
 
